1f59548c191bf7e5e92ba7ed50fd98eaec24dcb7
James Northrup <jim@keyframe.localdomain>
2009-05-30 14:32:14 -0700

    re-added the moved files.

---
+++ 1xio-server/src/main/java/alg/Pair.java
@@ -0,0 +1,54 @@
+package alg;
+
+import com.thoughtworks.xstream.XStream;
+
+//import static scene.SceneLayoutApp.XSTREAM;
+
+/**
+ * User: jim
+ * Date: May 13, 2009
+ * Time: 7:11:18 AM
+ */
+public class Pair<$1, $2> implements Tuple{
+    Object[] v;
+
+            static
+{
+        XSTREAM.aliasType("pair", Pair.class);
+    }
+
+    public Pair(Object... v) {
+        this.v = v;
+    }
+
+    public $1 $1() {
+        return ($1) v[0];
+
+    }
+
+    public $2 $2() {
+        return ($2) v[1];
+
+    }
+
+    public Pair<$1, $2> $1($1 $1) {
+
+        final $1 $11 = v.length > 1 ?
+                ($1) (v[0] = $1)
+                : ($1) (this.v = new Object[]
+                {$1, $2()})[0];
+        return this;
+
+    }
+
+    public Pair<$1, $2> $2($2 $2) {
+
+        final $2 $21 = v.length > 1 ?
+                ($2) (v[1] = $2)
+                : ($2) (this.v = new Object[]
+                {$1(), $2})[1];
+        return this;
+
+    }
+
+}
