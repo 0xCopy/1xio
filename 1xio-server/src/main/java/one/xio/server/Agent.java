31882fcc3619b8305b73647b9f1051c5d2a585ff
Laptops Anytime! <kiosk@laptopsanytime.(none)>
2009-12-02 01:31:11 -0800

    Agent has been genericized to allow overides on null-attachment situations, and to overide the handling of HttpMethod enum attachments as well

--- 1xio-server/src/main/java/one/xio/server/Agent.java
+++ 1xio-server/src/main/java/one/xio/server/Agent.java
@@ -1,37 +1,44 @@
 package one.xio.server;
 
 
-import one.xio.proto.*;
-import static one.xio.proto.ProtoUtil.*;
-
-import java.io.*;
-import java.net.*;
-import java.nio.channels.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 import alg.Pair;
-import javolution.util.FastList;
 import javolution.text.Text;
+import javolution.util.FastList;
+import one.xio.proto.HttpMethod;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.regex.Pattern;
+
+import static one.xio.proto.ProtoUtil.threadPool;
 //import javolution.text.Text;
 
 /**
  * Hello world!
  */
 public class Agent {
-    public boolean killswitch = false;
-    public ServerSocketChannel serverSocketChannel;
+    private boolean killswitch = false;
+    private ServerSocketChannel serverSocketChannel;
     //    public ByteBuffer token;
-    public int port = 8080;
+    private int port = 8080;
 
     // '$' as universal polymorphic default predicate,
     // when in doubt '$ $($...  $){} is the dominant generic predicate expression
 
     private static final HttpMethod $ = HttpMethod.$;
-    private FastList<Pair<String, String>> cfg = new FastList<Pair<String, String>>();
+    private List<Pair<String, String>> cfg = new FastList<Pair<String, String>>();
 
-    final Selector selector = Selector.open();
+    private final Selector selector = Selector.open();
+    private SelectionKey listenerKey;
+    private int bufferSize = 512;
 
     public Agent(CharSequence... args) throws IOException {
 
@@ -39,25 +46,20 @@ public class Agent {
             if (arg.charAt(0) == '-') {
                 final String[] v = Pattern.compile("=").split(arg, 2);
                 final Pair<String, String> pair = new Pair<String, String>(Text.intern(v[0].substring(1)).toString(), Text.valueOf(v[1]).toString());
-                cfg.add(pair);
+                getCfg().add(pair);
             }
         }
         Runnable runnable = new Runnable() {
             public void run() {
 
                 try {
-                    serverSocketChannel = ServerSocketChannel.open();
-                    final ServerSocket socket = serverSocketChannel.socket();
-                    serverSocketChannel.configureBlocking(false);
-                    socket.setPerformancePreferences(1, 9999, 0);
-                    socket.bind(new InetSocketAddress(port));
-                    socket.setReceiveBufferSize(512);
-                    
-                    final SelectionKey listenerKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+                    initSocket();
 
-                    while (!killswitch) {
-                        final int count = selector.select();
-                        final Set<SelectionKey> keys = selector.selectedKeys();
+                    setListenerKey(getServerSocketChannel().register(getSelector(), SelectionKey.OP_ACCEPT));
+
+                    while (!isKillswitch()) {
+                        final int count = getSelector().select();
+                        final Set<SelectionKey> keys = getSelector().selectedKeys();
 
                         for (Iterator<SelectionKey> i = keys.iterator(); i.hasNext();) {
                             final SelectionKey key = i.next();
@@ -67,10 +69,30 @@ public class Agent {
                                 try {
 
                                     final Object at = key.attachment();
+
+                                    if (at == null) {
+                                        if (key.isValid() && key.isWritable()) {
+                                            onWrite(key);
+                                        }
+
+                                        if (key.isValid() && key.isReadable()) {
+                                            onRead(key);
+                                        }
+
+                                        if (key.isValid() && key.isConnectable()) {
+                                            onConnect(key);
+                                        }
+
+                                        if (key.isValid() && key.isAcceptable()) {
+                                            onAccept(key);
+                                        }
+
+                                    }
                                     if (at instanceof Callable) {
 //                                        client.interestOps(0);
 //                                        threadPool.submit((Callable) at);
-                                        ((Callable) at).call();
+                                        final Callable callable = (Callable) at;
+                                        callable.call();
                                         continue;
                                     } else if (at instanceof Runnable) {
 //                                         client.interestOps(0);
@@ -81,30 +103,25 @@ public class Agent {
                                     } else {
 
                                         final HttpMethod m;
-                                        m = at == null
-                                                ? $
-                                                : (HttpMethod) (at instanceof Object[]
-                                                && ((Object[]) at)[0] instanceof HttpMethod
-                                                ? ((Object[]) at)[0]
-                                                : at instanceof HttpMethod
-                                                ? at
-                                                : $
-                                        );
+                                        m = (at == null) ?
+                                                $() : (HttpMethod) (at instanceof Object[] && ((Object[]) at)[0] instanceof HttpMethod ?
+                                                ((Object[]) at)[0] : at instanceof HttpMethod ?
+                                                at : $());
 
-                                        if (key.isWritable()) {
-                                            m.onWrite(key);
+                                        if (key.isValid() && key.isWritable()) {
+                                            onWrite(key, m);
                                         }
 
-                                        if (key.isReadable()) {
-                                            m.onRead(key);
+                                        if (key.isValid() && key.isReadable()) {
+                                            onRead(key, m);
                                         }
 
-                                        if (key.isConnectable()) {
-                                            m.onConnect(key);
+                                        if (key.isValid() && key.isConnectable()) {
+                                            onConnect(key, m);
                                         }
 
-                                        if (key.isAcceptable()) {
-                                            m.onAccept(key);
+                                        if (key.isValid() && key.isAcceptable()) {
+                                            onAccept(key, m);
                                         }
                                     }
 
@@ -127,14 +144,118 @@ public class Agent {
         threadPool.submit(runnable);
     }
 
+    private void onAccept(SelectionKey key) {
+
+    }
+
+    private void onConnect(SelectionKey key) {
+
+    }
+
+    private void onRead(SelectionKey key) {
+
+    }
+
+    private void onWrite(SelectionKey key) {
+
+    }
+
+    private void onAccept(SelectionKey key, HttpMethod m) {
+        m.onAccept(key);
+    }
+
+    private void onConnect(SelectionKey key, HttpMethod m) {
+        m.onConnect(key);
+    }
+
+    private void onRead(SelectionKey key, HttpMethod m) {
+        m.onRead(key);
+    }
+
+    private void onWrite(SelectionKey key, HttpMethod m) {
+        m.onWrite(key);
+    }
+
+
+    protected void initSocket() throws IOException {
+        setServerSocketChannel(ServerSocketChannel.open());
+        final ServerSocket socket = getServerSocketChannel().socket();
+        getServerSocketChannel().configureBlocking(false);
+        socket.setPerformancePreferences(1, 9999, 0);
+        socket.bind(new InetSocketAddress(getPort()));
+        setBufferSize(512);
+        socket.setReceiveBufferSize(getBufferSize());
+    }
+
     static public void main(String... a) throws IOException {
         final Agent agent = new Agent(a);
 
-        while (!agent.killswitch) {
+        while (!agent.isKillswitch()) {
             try {
                 Thread.sleep(10000);
             } catch (InterruptedException ignored) {
             }
         }
+    }
+
+    public boolean isKillswitch() {
+        return killswitch;
+    }
+
+    public void setKillswitch(boolean killswitch) {
+        this.killswitch = killswitch;
+    }
+
+    public ServerSocketChannel getServerSocketChannel() {
+        return serverSocketChannel;
+    }
+
+    public void setServerSocketChannel(ServerSocketChannel serverSocketChannel) {
+        this.serverSocketChannel = serverSocketChannel;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+
+    public List<Pair<String, String>> getCfg() {
+        return cfg;
+    }
+
+    public void setCfg(List<Pair<String, String>> cfg) {
+        this.cfg = cfg;
+    }
+
+    public Selector getSelector() {
+        return selector;
+    }
+
+    public SelectionKey getListenerKey() {
+        return listenerKey;
+    }
+
+    public void setListenerKey(SelectionKey listenerKey) {
+        this.listenerKey = listenerKey;
+    }
+
+    public static HttpMethod $() {
+        return get$();
+    }
+
+    public static HttpMethod get$() {
+        return $;
+    }
+
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    public void setBufferSize(int bufferSize) {
+        this.bufferSize = bufferSize;
     }
 }
