f126e7a3694efc8da4e01a2f87b6ce4871c4497d
Jim Northrup <northrup.james@gmail.com>
2012-05-22 02:01:13 -0700

    checkpoint

--- src/main/java/one/xio/HttpMethod.java
+++ src/main/java/one/xio/HttpMethod.java
@@ -292,9 +292,9 @@ public enum HttpMethod implements AsioVisitor {
     final Throwable throwable1 = throwable.fillInStackTrace();
     final StackTraceElement[] stackTrace = throwable1.getStackTrace();
     String ret = null;
-    for (int i = 2, end = min(stackTrace.length - 1, d); i < end; i++) {
+    for (int i = 2, end = min(stackTrace.length - 1, d); i <= end; i++) {
       StackTraceElement stackTraceElement = stackTrace[i];
-      ret = "\tat " + stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName() + "(" + stackTraceElement.getFileName() + ":" + stackTraceElement.getLineNumber() + ")\n";
+      ret += "\tat " + stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName() + "(" + stackTraceElement.getFileName() + ":" + stackTraceElement.getLineNumber() + ")\n";
 
     }
     return ret;
