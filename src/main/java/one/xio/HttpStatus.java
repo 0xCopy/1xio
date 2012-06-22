097f692dd0fd110e40437d36d6c9d44904a3a1f8
Jim Northrup <northrup.james@gmail.com>
2012-06-22 04:54:52 -0700

    make HttpStatus public

--- src/main/java/one/xio/HttpStatus.java
+++ src/main/java/one/xio/HttpStatus.java
@@ -18,7 +18,7 @@ package one.xio;
  * <p>5xx - Server Error
  * <br>Response status codes beginning with the digit"5" indicate cases in which the server is aware that it has erred or is incapable of performing the request. Except when responding to a HEAD request, the server SHOULD include an entity containing an explanation of the error situation, and whether it is a temporary or permanent condition. User agents SHOULD display any included entity to the user. These response codes are applicable to any request method.
  */
-enum HttpStatus {
+public enum HttpStatus {
 
     $100("Continue"),
     $101("Switching Protocols"),
